04018991c9b435262539a8550cd8239714b11dc1
tommy.delgado <tdelgado37.td@gmail.com>
2019-05-11 21:10:46 -0400

    first commit

---
+++ src/com/simple/calculator/Main.java
@@ -0,0 +1,54 @@
+package com.simple.calculator;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+//        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
+//        double[] rightVals = {50.0d, 92.0d, 175.0d, 3.0d};
+//        char[] opcode = {'d', 'a', 's', 'm'};
+//        double[]  results = new double[opcode.length];
+
+        Scanner in = new Scanner(System.in);
+        boolean keepGoing = true;
+//        MathEquation[] equations = new MathEquation[4];
+        while(keepGoing){
+            System.out.print("Please enter a number: ");
+            double num1 = in.nextDouble();
+            System.out.print("Please enter another number: ");
+            double num2 = in.nextDouble();
+            System.out.print("Please enter an operation ( a s d m): ");
+            String op = in.next();
+
+            MathEquation equation = create(num1, num2, op.charAt(0));
+            equation.execute();
+            System.out.println("The Result = " + equation.getResult());
+
+
+            System.out.print("Keep Going? (Y / N): ");
+            String loop = in.next();
+            if(loop.charAt(0) != 'Y')
+                if(loop.charAt(0) != 'y')
+                    keepGoing =  false;
+        }
+
+
+//        for(MathEquation x: equations){
+//            x.execute();
+//            System.out.print("The Result = ");
+//            System.out.println(x.getResult());
+//        }
+
+    }
+
+    public static MathEquation create(double leftVal, double rightVal, char opcode){
+        MathEquation equation = new MathEquation();
+        equation.setLeftVal(leftVal);
+        equation.setRightVal(rightVal);
+        equation.setOpcode(opcode);
+
+        return equation;
+    }
+}
---
+++ src/com/simple/calculator/MathEquation.java
@@ -0,0 +1,63 @@
+package com.simple.calculator;
+
+
+/**
+ * Created by Tommy Delgado
+ */
+public class MathEquation {
+    private double leftVal;
+    private double rightVal;
+    private char opcode;
+    private double result;
+
+    public double getLeftVal() {
+        return leftVal;
+    }
+
+    public void setLeftVal(double leftVal) {
+        this.leftVal = leftVal;
+    }
+
+    public double getRightVal() {
+        return rightVal;
+    }
+
+    public void setRightVal(double rightVal) {
+        this.rightVal = rightVal;
+    }
+
+    public char getOpcode() {
+        return opcode;
+    }
+
+    public void setOpcode(char opcode) {
+        this.opcode = opcode;
+    }
+
+    public void execute(){
+        switch(opcode) {
+            case 'a':
+                result = leftVal + rightVal;
+                break;
+            case 's':
+                result = leftVal - rightVal;
+                break;
+            case 'd':
+                result = rightVal != 0.0d ? leftVal / rightVal : 0.0d;
+                break;
+            case 'm':
+                result = leftVal * rightVal;
+                break;
+            default:
+                System.out.println("Error == invalid Opcode");
+                result = 0.0d;
+                break;
+        }
+    }
+
+    public double getResult(){
+        return this.result;
+    }
+
+
+}
