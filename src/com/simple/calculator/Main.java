a50fd6eae1f105cda31c333db5d7820c341d9f21
tommy.delgado <tdelgado37.td@gmail.com>
2019-05-13 21:13:00 -0400

    removed unused code

--- src/com/simple/calculator/Main.java
+++ src/com/simple/calculator/Main.java
@@ -5,11 +5,6 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-//        double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
-//        double[] rightVals = {50.0d, 92.0d, 175.0d, 3.0d};
-//        char[] opcode = {'d', 'a', 's', 'm'};
-//        double[]  results = new double[opcode.length];
 
         Scanner in = new Scanner(System.in);
         boolean keepGoing = true;
